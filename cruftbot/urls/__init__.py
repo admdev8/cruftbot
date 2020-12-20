81e2042cbf02aa3d70df7a1a1fa9d334e44d874b
Artem Malyshev <proofit404@gmail.com>
2020-12-21 02:14:14 +0300

    Add index page (#71).

--- cruftbot/urls/__init__.py
+++ cruftbot/urls/__init__.py
@@ -2,5 +2,7 @@ import actstream.urls
 from django.urls import include
 from django.urls import path
 
+from cruftbot.views.index import index
 
-urlpatterns = [path("activity/", include(actstream.urls))]
+
+urlpatterns = [path("activity/", include(actstream.urls)), path("", index)]
