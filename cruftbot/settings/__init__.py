40465dbfa2ef83257e1f9a8efa362d5f811d6e2b
Artem Malyshev <proofit404@gmail.com>
2021-01-07 02:58:02 +0300

    Setup django-csp (#15).

--- cruftbot/settings/__init__.py
+++ cruftbot/settings/__init__.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    "csp.middleware.CSPMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -96,3 +97,16 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+
+CSP_STYLE_SRC = ("'self'", "cdn.jsdelivr.net")
+
+CSP_SCRIPT_SRC = ("'self'", "cdn.jsdelivr.net")
+
+CSP_FONT_SRC = ("'self'",)
+
+CSP_IMG_SRC = ("'self'", "data:")
+
+CSP_DEFAULT_SRC = ("'none'",)
+
+# @todo #15 Configure CSP Violation Reports
+#  (https://django-csp.readthedocs.io/en/latest/reports.html).
