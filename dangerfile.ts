f67615485de654d00a3e14d0d14824024a04f9cf
Artem Malyshev <proofit404@gmail.com>
2021-02-14 23:06:19 +0300

    Setup Danger (#92).

---
+++ dangerfile.ts
@@ -0,0 +1,16 @@
+// @todo #92 Pull request title should start with capital letter.
+// @todo #92 Issue title should start with capital letter.
+// @todo #92 Commit message should start with capital letter.
+
+// @todo #92 Pull request title should ends with the dot.
+// @todo #92 Issue title should ends with the dot.
+// @todo #92 Commit message should ends with the dot.
+
+// @todo #92 Commit title SHOULD contain issue number.
+// @todo #92 Pull request title SHOULD NOT contain issue number.
+// @todo #92 Pull request body SHOULD contain issue number.
+
+// @todo #92 Multiline commit message should separate title with new line.
+
+// @todo #92 Pull request should not have labels.
+// @todo #92 Issue should not have labels.
