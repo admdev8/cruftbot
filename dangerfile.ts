d1cc556ddcc35ee8209e425222e291a9ce74341a
Artem Malyshev <proofit404@gmail.com>
2021-04-17 10:14:12 +0300

    Pull request body SHOULD contain issue number (#132).

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,24 +3,45 @@ export default async (): undefined => {
     fail("Pull request title should start with capital letter.");
     return;
   }
-// @todo #92 Issue title should start with capital letter.
-// @todo #92 Commit message should start with capital letter.
+
+  // @todo #92 Issue title should start with capital letter.
+
+  // @todo #92 Commit message should start with capital letter.
+
   if (!/\.$/.test(danger.github.pr.title)) {
     fail("Pull request title should ends with the dot.");
     return;
   }
-// @todo #92 Issue title should ends with the dot.
-// @todo #92 Commit message should ends with the dot.
 
-// @todo #92 Commit title SHOULD contain issue number.
-// @todo #92 Pull request title SHOULD NOT contain issue number.
-// @todo #92 Pull request body SHOULD contain issue number.
+  // @todo #92 Issue title should ends with the dot.
 
-// @todo #92 Multiline commit message should separate title with new line.
+  // @todo #92 Commit message should ends with the dot.
+
+  // @todo #92 Commit title SHOULD contain issue number.
+
+  // @todo #92 Pull request title SHOULD NOT contain issue number.
+
+  const pullRequestTest = danger.github.pr.body.match(/^#(\d+)$/);
+
+  if (!pullRequestTest) {
+    fail("Pull request body SHOULD contain issue number.");
+    return;
+  }
+
+  const issueNumber = pullRequestTest[1];
+
+  const issueJSON = await danger.github.api.issues.get({
+    owner: danger.github.thisPR.owner,
+    repo: danger.github.thisPR.repo,
+    issue_number: parseInt(issueNumber),
+  });
+
+  // @todo #92 Multiline commit message should separate title with new line.
 
   if (danger.github.pr.labels.length > 0) {
     fail("Pull request should not have labels.");
     return;
   }
-// @todo #92 Issue should not have labels.
+
+  // @todo #92 Issue should not have labels.
 };
